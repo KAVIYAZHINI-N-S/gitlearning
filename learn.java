161833298385ef2bade845d513e302e1d5301df9
KAVIYAZHINI-N-S <yazhini27504@gmail.com>
2025-09-27 11:16:14 +0530

    a modified

--- learn.java
+++ learn.java
@@ -1,7 +1,7 @@
 public class learn{
     public static void main(String[] args) {
         //Scanner st = new Scanner(System.in);
-        int a = 16;
+        int a = 15;
         int b = 20;
         System.out.print(a+b);
     }
