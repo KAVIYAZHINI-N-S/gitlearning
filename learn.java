94af499e732bc87fad9e94c6e7029e35f53da9b3
KAVIYAZHINI-N-S <yazhini27504@gmail.com>
2025-09-27 12:03:17 +0530

    b as 2000

--- learn.java
+++ learn.java
@@ -2,7 +2,7 @@ public class learn{
     public static void main(String[] args) {
         //Scanner st = new Scanner(System.in);
         int a = 30;
-        int b = 20;
+        int b = 2000;
         System.out.print(a+b);
         System.out.print(a*b);
     }
