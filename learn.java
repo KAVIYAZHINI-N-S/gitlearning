145159e1031f7f6c271ff8b6ddd632f3aa422741
KAVIYAZHINI N S <yazhini27504@gmail.com>
2025-09-27 11:02:12 +0530

    added varaible b & printed a+b

--- learn.java
+++ learn.java
@@ -2,6 +2,7 @@ public class learn{
     public static void main(String[] args) {
         //Scanner st = new Scanner(System.in);
         int a = 16;
-        System.out.print(a);
+        int b = 20;
+        System.out.print(a+b);
     }
 }
